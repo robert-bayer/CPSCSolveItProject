2197d5819792f37b9090dc41670343092bc22762
Robert Bayer <roberto.bearini@gmail.com>
2021-11-30 18:35:34 -0500

    Project manipulation and console support

---
+++ src/PMConsole.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class PMConsole {
+
+    public static void main(String[] args){
+        String Name;
+        List<String> projects = new ArrayList<>();
+        Scanner stdinScanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Project Manager!\nThis program is designed to help you separate work and home while you work from home.\nIn here, you will be able to start, manage, and archive projects as you work on them.\nProjects can be as simple as reading a paper, to curing cancer.\nEach project has the ability to save tasks or goals, set a deadline for the project, and set task statuses.");
+
+        System.out.print("To get stared, let's get started by having you tell us your name: ");
+        Name = stdinScanner.next();
+        System.out.println("Hello, " + Name + "!\nLet's get started by creating a Project!");
+
+        System.out.print("What would you like to name your first project: ");
+        Project newProject = new Project(stdinScanner.next());
+
+        System.out.println("Awesome! Try Listing some tasks. You can continue listing task, and when you're ready to move on type 'done'.");
+        String input = null;
+        do{
+            System.out.print("Enter a task:");
+            input = stdinScanner.nextLine();
+            if (input.equals("done")){
+                break;
+            }
+            if(input != null) {
+                newProject.addTask(input);
+            }
+        }
+        while (!input.equals("done"));
+
+        System.out.println("Fantastic! By default, all projects are set to a status of 'Not Started'. You will have the option to edit each later.");
+
+        System.out.println("I will now display all of the tasks:");
+        System.out.println(newProject.taskToString());
+        System.out.print("What task do you want to delete: ");
+        newProject.removeTask(stdinScanner.nextLine());
+        System.out.println("Here is the new list: " + newProject.taskToString());
+        System.out.println("Let's try saving it to a file.");
+        newProject.saveToFile();
+        System.out.println("This concludes our demo.");
+    }
+}
--- src/Project.java
+++ src/Project.java
@@ -1,2 +1,91 @@
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.io.IOException;
+
+
+//Project has tasks, deadline,
+
+//When a project is open store all progess, tasks, and info in ArrayList.  Upon the closing of project, save the Array list in a file.
+//To open project read the file and have each line a different task in ArrayList
+
 public class Project {
+    FileWriter out = null;
+    private String Name;
+    List<String> Tasks = new ArrayList<>();
+    List<String> TaskStatus = new ArrayList<>();
+    private String Deadline;
+
+    public Project(String Name){
+        this.Name = Name;
+    }
+
+    public void closeWriter(Writer file){
+        try{
+            if(file != null){
+                file.close();
+            }
+        }
+        catch(IOException e){
+            System.err.println("ERROR: There was an IOExeption when trying to close the file: " + e.getMessage());
+        }
+    }
+
+
+    public void setName(String Name){
+        this.Name = Name;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public void addTask(String task){
+        Tasks.add(task);
+        TaskStatus.add("Not Started");
+    }
+
+    public List<String> displayTasks(){
+        return Tasks;
+    }
+
+    public void removeTask(String task){
+        int counter = 0;
+        for(String i : Tasks) {
+            if (i.equals(task)) {
+                Tasks.remove(counter);
+            }
+            counter++;
+        }
+    }
+
+    public void saveToFile() {
+        try {
+            out = new FileWriter(Name);
+            out.write(Name + "\n");
+
+            for (String task : Tasks) {
+                out.write(task + "\n");
+            }
+        }
+        catch (IOException e) {
+            System.err.println("ERROR: There was an IOException when writing to file: " + e.getMessage());
+        }
+        catch (Exception e) {
+            System.err.println("ERROR: There was an Exception when writing to file: " + e.getMessage());
+        }
+        finally{
+            closeWriter(out);
+        }
+    }
+
+    public String taskToString(){
+        String TaskString = "";
+        for(String task : Tasks){
+            TaskString += task + ", ";
+        }
+        return TaskString;
+    }
 }

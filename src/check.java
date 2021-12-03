2d3d05189443e1b35ce39646b10b13071e4b11ee
F0recast1254 <95315760+F0recast1254@users.noreply.github.com>
2021-12-03 15:06:22 -0500

    Add files via upload

--- src/check.java
+++ src/check.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 public class check {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         long time = System.currentTimeMillis();
         System.out.println(time);
         long time2 = time + (28800);
-        while (System.currentTimeMillis()!= time2){
+        int FunnyNumber = 0;
+        Scanner Xbox = new Scanner(System.in);
+        while (FunnyNumber != 1) {
+            System.out.println("Yeah that fits.");
+            Xbox.next();
             System.out.println(timecheck(time2));
+            if(timecheck(time2).equals("Time's up!")) {
+                FunnyNumber = 1;
+            }
         }
     }
-    public static String timecheck(long fuck){
-        long Remainingtime= fuck - System.currentTimeMillis();
-        return "Current time remaining: "+Remainingtime;
+
+    public static String timecheck(long fuck) {
+        if (System.currentTimeMillis() <= fuck) {
+            long Remainingtime = fuck - System.currentTimeMillis();
+            long RemainingSeconds = Remainingtime / 1000;
+            return "Current time remaining: " + RemainingSeconds + " seconds.";
+        } else {
+            return "Time's up!";
+        }
     }
 }

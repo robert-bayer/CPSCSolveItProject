925701dc7bc4dc46f84c881405eba24f0c4d852a
F0recast1254 <95315760+F0recast1254@users.noreply.github.com>
2021-12-07 15:27:38 -0500

    Add files via upload

--- src/User.java
+++ src/User.java
@@ -1,7 +1,15 @@
 import java.io.FileWriter;
 import java.io.IOException;
-
+/**
+ * @author Andrew Gonye
+ * @author Robert Bayer
+ * @author Clayton Everitt
+ */
+/**
+ * User sets a username and password to a user object as well as returns it.
+ */
 public class User {
+
     String Password;
     String Username;
     public User(String Username, String Password){
@@ -9,19 +17,41 @@ public class User {
         setPassword(Password);
     }
     public User(){}
+
+    /**
+     * sets a username
+     * @param User username that was input
+     */
     public void setUsername(String User){
+        //setter for username
         Username = User;
     }
+
+    /**
+     * sets a password to a user
+     * @param Pass password that was input
+     */
     public void setPassword(String Pass){
+        //setter for password
         Password = Pass;
     }
+
+    /**
+     * returns the password of the user
+     * @return password string
+     */
     public String getPassword(){
+        //getter for password
         return Password;
     }
+
+    /**
+     * returns the username of the User
+     * @return username string
+     */
     public String getUsername(){
+        //getter for username
         return Username;
     }
-
-
 
 }
